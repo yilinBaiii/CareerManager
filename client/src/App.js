d6ff0d64582e61e45638fcfd6bce67ae600deb41
Yilinbai <yilinbaiii@gmail.com>
2023-02-25 18:35:52 -0500

    Add Landing Wrapper

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,8 @@
 import Landing from "./pages/Landing";
 
-
 function App() {
   return (
     <div>
-      <h1> Career Manager</h1>
-
       <Landing />
     </div>
   )
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,32 +1,37 @@
 import React from 'react'
-// import logo from "../images/logo.ico"
+import Space from "../images/Space.png"
 import walkerLogo from "../images/WalkerLogo.png"
 import CM_Text from "../images/CM_Text.png"
 import "./Landing.css"
-const Landing = () => {
-  return (
-    <main>
-        <nav>
-        <div id="logo-div" >
-            <img src= {walkerLogo} alt='walkerLogo' height = "50px"></img>
-            <img src= {CM_Text} alt='CM_Text' height = "50px"></img>
-        </div>
-            
-        </nav>
-        <div className = "container page">
-            <div className='info'>
-            <h1>
-                Career<span> Managing</span>App
-            </h1>
-            <p>
-                hello world
+import Wrapper from '../wrappers/LandingPage';
 
-            </p>
-            <button className='btn btn-hero'>Login/Register</button>
+const Landing = () => {
+    return (
+        <Wrapper>
+            <nav>
+                <div id="logo-div" >
+                    <img src={walkerLogo} alt='walkerLogo' height="50px"></img>
+                    <img src={CM_Text} alt='CM_Text' height="50px"></img>
+                </div>
+            </nav>
+            <div className="container page">
+                {/* info */}
+                <div className='info'>
+                    <h1>
+                        Career<span> Managing</span> App
+                    </h1>
+                    <p>
+                        This is a job search tool that enables job seekers to efficiently manage
+                        and track their job search progress in a centralized location.
+                        Leveraging the power of MongoDB, Express, React, and Node.js,
+                        this full-stack application provides a seamless experience for users.
+                    </p>
+                    <button className='btn btn-hero'>Login/Register</button>
+                </div>
+                <img src={Space} alt="Space" className='img main-img'></img>
             </div>
-        </div>
-    </main>
-  )
+        </Wrapper>
+    )
 }
 
 export default Landing
