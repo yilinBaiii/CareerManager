29b572841d1b35762e43526ffae7e9890a725f26
Yi Zhao <yzhao156@uottawa.ca>
2023-02-28 22:07:13 -0500

    Clean Code

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,13 @@ import { Register, Landing, Dashboard, Error } from "./pages/index"
 function App() {
   return (
     <BrowserRouter>
-      {/* <nav>
-      <Link to='/'>Dashboard</Link>
-      <Link to='/register'>Register</Link>  
-      <Link to='/landing'>Home</Link>
-    </nav> */}
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/landing" element={<Landing />} />
-      <Route path="*" element={<Error />} />
-    </Routes>
-</BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/landing" element={<Landing />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
