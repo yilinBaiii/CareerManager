6d8ae59a2c3f496af18af60c00b30719e4cc6682
Yilinbai <yilinbaiii@gmail.com>
2023-03-04 15:28:27 -0500

    Add module

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import notFoundMiddleware from './middleware/not-found.js';
+
+const app = express();
+
+
+//middleware
+// notFoundMiddleware
+
+app.get('/', (req, res) => {
+    res.send("Welcome")
+})
+
+app.use(notFoundMiddleware);
+const port = (process.env.PORT || 8000);
+app.listen(port, () => 
+    console.log(`Server is listening on port ${port} ...`)
+)
